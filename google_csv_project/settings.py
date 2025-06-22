12b25158f230bdd4f4765a4890025270158c1911
Vaibhav Singh <124482379+white-heart-05@users.noreply.github.com>
2025-06-22 14:19:56 +0530

    Update settings.py

--- google_csv_project/settings.py
+++ google_csv_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--#(o#5p@$1k%-gb96y#7(nl=nuh5loulve5q2=wg3ja)=-&4ai
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
